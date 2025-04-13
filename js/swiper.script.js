68d559e43743e96a3c2cd83315234a70ec86aaff
EdOc-PS <eeuardooctavio@gmail.com>
2025-04-13 16:40:36 -0300

    feat: finish

---
+++ js/swiper.script.js
@@ -0,0 +1,32 @@
+new Swiper('.container_carrossel', {
+    loop: true,
+
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    breakpoints: {
+        0:{
+            slidesPerView: 2,
+            spaceBetween: 15
+        },
+        768:{
+            slidesPerView: 3,
+            spaceBetween: 20
+        },
+        1024:{
+            slidesPerView: 5,
+            spaceBetween: 20
+        }
+    }
+  })
