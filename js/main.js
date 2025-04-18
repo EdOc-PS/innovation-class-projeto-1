aa62aa2d5ab78272222f18dd8a742821a2b25992
EdOc-PS <eeuardooctavio@gmail.com>
2025-04-18 16:09:58 -0300

    fix: change the footer and header

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const variable = {
         bodyAllCategories: document.querySelector('.container_allCategories'),
         bodyDepartament: document.querySelector('.container_departament'),
         btnHeaderExpand: document.querySelectorAll('.btn_menu'),
+        iconBtnMenu: document.querySelector('.fi.fi-br-menu-burger'),
         liCategories: document.querySelectorAll('.li_categories.comun'),
         btnSearch: document.getElementById('btn_search'),
         inputSearch: document.getElementById("input_search"),
@@ -17,7 +18,8 @@ const variable = {
 
     },
     footer: {
-        details: document.querySelectorAll(".details_about")
+        details: document.querySelectorAll(".details_about"),
+        elementsHeading: document.querySelectorAll("summary .elements_heading")
     }
 };
 
@@ -27,20 +29,22 @@ function menuExpanded() {
         button.addEventListener('click', () => {
             variable.header.headerExpand.classList.toggle("expanded");
             variable.header.bodyAllCategories.style.display = 'flex';
+            variable.header.iconBtnMenu.classList.toggle("fi-br-x");
         });
+        if (window.innerWidth >= 768) {
+            button.addEventListener('mouseover', () => {
+                variable.header.headerExpand.classList.add("expanded");
+                variable.header.bodyAllCategories.style.display = 'flex';
+                variable.header.bodyDepartament.style.display = 'none';
+                button.style.color = "#005CFF";
+            });
 
-        button.addEventListener('mouseover', () => {
-            variable.header.headerExpand.classList.add("expanded");
-            variable.header.bodyAllCategories.style.display = 'flex';
-            variable.header.bodyDepartament.style.display = 'none';
-            button.style.color = "#005CFF";
-        });
-
-        variable.header.bodyAllCategories.addEventListener('mouseleave', () => {
-            variable.header.headerExpand.classList.remove("expanded");
-            variable.header.btnHeaderExpand.forEach(btn => btn.style.color = "");
-            variable.header.allCategories.style.display = "none";
-        });
+            variable.header.bodyAllCategories.addEventListener('mouseleave', () => {
+                variable.header.headerExpand.classList.remove("expanded");
+                variable.header.btnHeaderExpand.forEach(btn => btn.style.color = "");
+                variable.header.allCategories.style.display = "none";
+            });
+        }
     });
 }
 
@@ -98,15 +102,27 @@ function toggleAccordion() {
         }
     });
 }
+// Alterar os elementos headers no footer
+function toggleHeading() {
+    variable.footer.elementsHeading.forEach(e => {
+        if (window.innerWidth >= 768) {
+            e.innerHTML = '&lt;h4&gt;';
+        } else {
+            e.innerHTML = '&lt;h5&gt;';
+        }
+    });
+}
 
-
-window.addEventListener("resize", toggleAccordion);
+window.addEventListener("resize", () => {
+    toggleAccordion();
+    toggleHeading();
+});
 
 menuExpanded();
 categoryHover();
 searchMenssage();
 departamentExpanded();
 toggleAccordion();
-
+toggleHeading();
 
 
--- js/swiper.script.js
+++ js/swiper.script.js
@@ -1,32 +1,49 @@
 new Swiper('.container_carrossel', {
-    loop: true,
+  loop: true,
 
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-      dynamicBullets: true,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+
     },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20
     },
-    
-    breakpoints: {
-        0:{
-            slidesPerView: 2,
-            spaceBetween: 15
-        },
-        768:{
-            slidesPerView: 3,
-            spaceBetween: 20
-        },
-        1024:{
-            slidesPerView: 5,
-            spaceBetween: 20
-        }
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20
     }
-  })
+  }
+})
+
+new Swiper('.mobile-banner', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
